fdb12bfed0c48d5db4eb02cb13ab99202058ab04
VivekWesley <vivekjohnwesley@gmail.com>
2020-07-13 02:51:48 +0530

    Updates

---
+++ precache-manifest.66965637179643df2b2e4dcd997eb2c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859cb523a44b5df409254e4198828fc1",
+    "url": "/Rick-and-Morty-Episode/index.html"
+  },
+  {
+    "revision": "e9e56bf20225a8a41e9e",
+    "url": "/Rick-and-Morty-Episode/static/css/main.b5768d8c.chunk.css"
+  },
+  {
+    "revision": "e9f5837c402721616f51",
+    "url": "/Rick-and-Morty-Episode/static/js/2.6c355c2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rick-and-Morty-Episode/static/js/2.6c355c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e56bf20225a8a41e9e",
+    "url": "/Rick-and-Morty-Episode/static/js/main.70dd3acd.chunk.js"
+  },
+  {
+    "revision": "25ef03e7adf2da7ad3ba",
+    "url": "/Rick-and-Morty-Episode/static/js/runtime-main.2391794f.js"
+  }
+]);
